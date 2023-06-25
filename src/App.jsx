20c13c813b8bb5be472a8081b202e74107b3a53b
sujitmemane <officialsujitmemane@gmail.com>
2023-06-25 18:57:39 +0530

    Added Firebase Data Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,75 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { uiSliceAction } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+let initialTime = true;
+
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.showCart);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch("https://advanceredux-87236-default-rtdb.firebaseio.com/cart.json", {
-      method: "POST",
-      body: JSON.stringify(cart),
+    const sendingCartData = async () => {
+      dispatch(
+        uiSliceAction.showNotification({
+          status: "Pending",
+          title: "Sending...",
+          message: "Sending Data to Cart",
+        })
+      );
+      const response = await fetch(
+        "https://advanceredux-87236-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "POST",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending Data to Cart Failed");
+      }
+      dispatch(
+        uiSliceAction.showNotification({
+          status: "success",
+          title: "Data Sent Successfully!",
+          message: "The data is sent Sucessfully!",
+        })
+      );
+      console.log(notification);
+    };
+
+    if (initialTime) {
+      initialTime = false;
+      return;
+    }
+    sendingCartData().catch((error) => {
+      uiSliceAction.showNotification({
+        status: "error",
+        title: "Data Sent Failed!",
+        message: error.message,
+      });
     });
   }, [cart]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -4,10 +4,18 @@ const uiSlice = createSlice({
   name: "ui",
   initialState: {
     showCart: false,
+    notification: null,
   },
   reducers: {
     toggle(state) {
       state.showCart = !state.showCart;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
